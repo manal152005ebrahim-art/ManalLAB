1ff224a705d0a10ca51c53ca0edb954b7b6f557a
lenovo <manal152005ebrahim@gmail.com>
2025-11-19 17:40:16 +0300

    lab1

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import Arrayss.Arrayss;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner A=new Scanner(System.in);
+        Arrayss arr =new Arrayss();
+       // int []numbers=new int[5];
+        int [] numbers={1,2,3,4};
+        arr.Traversal2(numbers);
+       // arr.Traversal(numbers);
+
+        System.out.println("please enter element");
+        numbers[0]=A.nextInt();
+
+
+        //  int [] numbers= new int[5];
+      //  System.out.println(Arrays.toString(numbers));
+
+        //int [] number ={1,2,3,4};
+       // for(int i=0;i<number.length;i++)
+           // System.out.println(number[i]);
+
+       // for(int e:number)
+          //  System.out.println(e);
+
+
+ // int [] number={1,2,3};
+  //number[0]=6;
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
