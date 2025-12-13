e26e94c27fe70e5c01e11a484f2d0f5076379125
lenovo <manal152005ebrahim@gmail.com>
2025-12-13 21:58:49 +0300

    singlyLinkedList

---
+++ src/singlyLinkedList/Main.java
@@ -0,0 +1,17 @@
+package singlyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        singlyLinkedList<Integer> List=new singlyLinkedList<Integer>();
+        List.addFrist(10);
+        List.addFrist(20);
+        List.addFrist(30);
+        List.addFrist(40);
+        List.removeFrist();
+        List.display();
+    }
+    }
+
+
+
+
---
+++ src/singlyLinkedList/singlyLinkedList.java
@@ -0,0 +1,107 @@
+package singlyLinkedList;
+
+public class singlyLinkedList<E> {
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
+
+    public int size() {
+        return size;
+
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public void addFrist(E data) {
+        Node<E> newNode = new Node(data, head);
+        head = newNode;
+        if (isEmpty())
+        {
+        tail = head;
+    }
+
+    size++;
+}
+public E getFrist(){
+        if(isEmpty())return null;
+        return head.getData();
+
+}
+
+    public E removeFrist(){
+        if(isEmpty())return null;
+        E delete= head.getData();
+        head=head.getNext();
+        size--;
+        if(isEmpty())
+            tail=head;
+
+        return delete;}
+
+public void addLast(E data) {
+    Node<E> newNode=new Node(data,null);
+    if(isEmpty())
+        head=newNode;
+    else
+        tail.setNext(newNode);
+
+    tail=newNode;
+    size++;
+
+
+}
+    public E getLast(){
+        if(isEmpty())return null;
+        return tail.getData();
+
+    }
+            public void display(){
+            Node<E> temp=head;
+            while (temp!=null){
+                System.out.println(temp.getData()+"--------->");
+                temp=temp.getNext();}
+            System.out.println("null");
+        }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    class Node<E>{
+        private E data;
+        private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
+    }
+
